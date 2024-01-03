95e65d9d9fefdaa43c105eb683aad5d869ed4402
Nicolas BERNA <nicolas_github@berna.fr>
2024-01-03 18:39:55 +0100

    autogen dh

--- config/run.sh
+++ config/run.sh
@@ -3,6 +3,11 @@
 # Personnalisation de la configuration
 sed -i "s/#connect =/connect = host=${DB_HOST} dbname=${DB_DATABASE} user=${DB_USER} password=${DB_PASSWORD}/g" /etc/dovecot/dovecot-sql.conf.ext
 
+
+    if [ ! -f "/etc/ssl/dovecot/dh.pem" ]; then
+        openssl dhparam -out /etc/ssl/dh.pem 2048
+    fi
+
 #Start services
 
 exec /usr/sbin/dovecot -F
