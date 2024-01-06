c655af2aa3bb7557edbde65ee455a23c8fa30430
Nicolas BERNA <nicolas_github@berna.fr>
2024-01-06 20:25:38 +0100

    compile sieve on container start

--- config/run.sh
+++ config/run.sh
@@ -3,10 +3,16 @@
 # Personnalisation de la configuration
 sed -i "s/#connect =/connect = host=${DB_HOST} dbname=${DB_DATABASE} user=${DB_USER} password=${DB_PASSWORD}/g" /etc/dovecot/dovecot-sql.conf.ext
 
+if [ ! -f "/etc/ssl/dovecot/dh.pem" ]; then
+   openssl dhparam -out /etc/ssl/dh.pem 2048
+fi
 
-    if [ ! -f "/etc/ssl/dovecot/dh.pem" ]; then
-        openssl dhparam -out /etc/ssl/dh.pem 2048
-    fi
+# Compile Sieve files if doesn't exist
+if [ ! -f "/etc/dovecot/sieve/SpamToJunk.sieve" ]; then
+   sievec /etc/dovecot/sieve/
+fi
+
+
 
 #Start services
 
--- config/sieve/SpamToJunk.sieve
+++ config/sieve/SpamToJunk.sieve
@@ -2,6 +2,9 @@ require ["fileinto", "mailbox"];
 # fileinto: for putting mail into a imap folder
 # mailbox: for creating imap folder if not exists
 #
+
+# File compiled by run.sh
+
 if header :contains "X-Spam-Flag" "YES" {
     # move mail into Folder Spam, create folder if not exists
     fileinto :create "Junk";
