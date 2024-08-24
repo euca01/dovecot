41c5d82b5319dada3a977b7fead9b0d59637a4dd
Nicolas BERNA <nicolas_github@berna.fr>
2024-08-25 01:43:48 +0200

    fix file path gen for DH

--- config/run.sh
+++ config/run.sh
@@ -4,7 +4,7 @@
 sed -i "s/#connect =/connect = host=${DB_HOST} dbname=${DB_DATABASE} user=${DB_USER} password=${DB_PASSWORD}/g" /etc/dovecot/dovecot-sql.conf.ext
 
 if [ ! -f "/etc/ssl/dovecot/dh.pem" ]; then
-   openssl dhparam -out /etc/ssl/dh.pem 2048
+   openssl dhparam -out /etc/ssl/dovecot/dh.pem 2048
 fi
 
 # Compile Sieve files if doesn't exist
